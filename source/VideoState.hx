fd03d2f8100c50c96498359da7ecadfa8a05ff6d
SoyManuelGame <95936700+SoyManuelGame@users.noreply.github.com>
2021-12-27 14:42:38 -0500

    Add files via upload

--- source/VideoState.hx
+++ source/VideoState.hx
@@ -45,10 +45,6 @@ class VideoState extends MusicBeatState
 		
 		leSource = source;
 		transFunction = toTrans;
-		if (frameSkipLimit != -1 && GlobalVideo.isWebm)
-		{
-			WebmPlayer.SKIP_STEP_LIMIT = frameSkipLimit;	
-		}
 	}
 	
 	override function create()
